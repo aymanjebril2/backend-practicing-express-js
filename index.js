9e2733fecfa7bb62f44697bb86c7b43deef1d347
Ayman Omer <aymangebrilomer@gmail.com>
2021-03-31 01:14:35 -0400

    add jjison data to the route

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ import path from "path";
 
 const app = express();
 
+const members = [
+  {
+    id: 1,
+    name: "ayman omer ",
+    email: "ayman@ayman.com",
+    status: "inactive",
+  },
+  {
+    id: 2,
+    name: "shafee ahmed",
+    email: "shfee@shfee.com",
+    status: "inactive",
+  },
+  {
+    id: 1,
+    name: "jahzeal isreal ",
+    email: "jahzeal@jahzeal.com",
+    status: "inactive",
+  },
+];
+
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
 const __dirname = path.resolve(path.dirname(""));
 app.use(express.static(path.join(__dirname, "PAGES")));
 
