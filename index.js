657f295596ce55376c2b26da517723499fde23a9
Ayman Omer <aymangebrilomer@gmail.com>
2021-03-31 17:36:21 -0400

    how to get singal membres if there will be ok if is not will give bad req 400

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ app.get("/api/members/:id", (req, res) => {
   // here to check pramas on the req
   //   res.send(req.params.id);
   /// here how to go and got singal on by using filter mathood and and the req send it as string we have to change to number
-  res.json(members.filter((member) => member.id === +req.params.id));
+
+  /// and here how to get some mathod if ture will give the members if is not will send wrong req 400
+  const found = members.some((member) => member.id === +req.params.id);
+  if (found) {
+    res.json(members.filter((member) => member.id === +req.params.id));
+  } else {
+    res.status(400).json({ msg: "Members not Found " });
+  }
 });
 
 const __dirname = path.resolve(path.dirname(""));
